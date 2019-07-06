04226bbae4ed117275deaf037c758368678b05b9
swaelo <harleylaurie@rocketmail.com>
2019-07-07 00:26:43 +1000

    added all the new PacketHandler and PacketSender classes used for handling networking communication between the client and server

---
+++ Assets/Scripts/Networking/PacketHandlers/PlayerManagementPacketHandler.cs
@@ -0,0 +1,46 @@
+﻿// ================================================================================================================================
+// File:        PlayerManagementPacketHandler.cs
+// Description:	Handles packets from the server with stuff like updated position values for other ingame player characters
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+
+public class PlayerManagementPacketHandler : MonoBehaviour
+{
+    //Singleton Instance
+    public static PlayerManagementPacketHandler Instance = null;
+    void Awake() { Instance = this; }
+
+    //Handles moving one of the other players ingame characters to a new updated location
+    public static void HandlePlayerUpdate(ref NetworkPacket Packet)
+    {
+        //Read the relevant values from the network packet
+        string CharacterName = Packet.ReadString();
+        Vector3 CharacterPosition = Packet.ReadVector3();
+
+        //Pass these values onto the remote player handler and it will move the correct player to its new target location
+        RemotePlayerHandler.Instance.UpdatePlayerPosition(CharacterName, CharacterPosition);
+    }
+
+    //Handles instructions to spawn a newly connected game clients player character into our game world
+    public static void HandleSpawnPlayer(ref NetworkPacket Packet)
+    {
+        //Read the relevant values from the network packet
+        string CharacterName = Packet.ReadString();
+        Vector3 CharacterPosition = Packet.ReadVector3();
+
+        //Use the remote player manager to spawn this remote player character into our game world
+        RemotePlayerHandler.Instance.AddRemotePlayer(CharacterName, CharacterPosition);
+    }
+
+    //Handles instructions to despawn some other dead clients player character from our game world
+    public static void HandleRemovePlayer(ref NetworkPacket Packet)
+    {
+        //Read the relevant data values from the network packet
+        string CharacterName = Packet.ReadString();
+
+        //Use the remote player manager to despawn this dead clients player character from our game world
+        RemotePlayerHandler.Instance.RemoveRemotePlayer(CharacterName);
+    }
+}
---
+++ Assets/Scripts/Networking/PacketSenders/PlayerManagementPacketSender.cs
@@ -0,0 +1,28 @@
+﻿// ================================================================================================================================
+// File:        PlayerManagementPacketSender.cs
+// Description:	Sends packets to the game server providing our local character controllers updated position values
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+
+public class PlayerManagementPacketSender : MonoBehaviour
+{
+    //Singleton Instance
+    public static PlayerManagementPacketSender Instance = null;
+    void Awake() { Instance = this; }
+
+    public void SendPlayerUpdate(Vector3 CharacterPosition)
+    {
+        //Create a new NetworkPacket object to store the data for this player update
+        NetworkPacket Packet = new NetworkPacket();
+
+        //Write the relevant data values into the network data
+        Packet.WriteType(ClientPacketType.PlayerUpdate);
+        Packet.WriteString(GameState.Instance.CurrentCharacterName);
+        Packet.WriteVector3(CharacterPosition);
+
+        //Add the new NetworkPacket to the outgoing packets queue
+        ConnectionManager.Instance.PacketQueue.QueuePacket(Packet);
+    }
+}
