61908d2a8ee754e3e952c4f3effb122dfdae114f
Swaelo <harleylaurie@rocketmail.com>
2019-11-02 06:07:20 +1000

    added new scene that players are sent to upon losing connection to the game server

---
+++ Assets/Scripts/Interface/LoadSceneButton.cs
@@ -0,0 +1,18 @@
+﻿// ================================================================================================================================
+// File:        LoadSceneButton.cs
+// Description: Gives UI buttons the functionality to load into a different scene
+// Author:      Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public string SceneName = "";
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
