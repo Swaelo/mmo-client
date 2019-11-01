ff8483a9e0e29834c5b97f49c9f01344604b8db0
Swaelo <harleylaurie@rocketmail.com>
2019-11-02 06:08:45 +1000

    added new memory monitor debug script which displays some useful information onto the UI during runtime

---
+++ Assets/Scripts/Debug/MemoryMonitor.cs
@@ -0,0 +1,28 @@
+﻿// ================================================================================================================================
+// File:        MemoryMonitor.cs
+// Description: Displays amount of memory currently used, and amount of memory that is free on the UI
+// Author:      Harley Laurie https://blog.kongregate.com/unity-webgl-memory-and-performance-optimization/
+// ================================================================================================================================
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryMonitor : MonoBehaviour
+{
+    public Text UIText;
+
+    private void Start()
+    {
+        int TotalSystemMemory = SystemInfo.systemMemorySize;
+        int TotalGraphicsMemory = SystemInfo.graphicsMemorySize;
+        string ProcessorType = SystemInfo.processorType;
+        int ProcessorCount = SystemInfo.processorCount;
+        int ProcessorFrequency = SystemInfo.processorFrequency;
+
+        UIText.text = "Total System Memory: " + TotalSystemMemory + "\n" +
+            "Total Graphics Memory: " + TotalGraphicsMemory + "\n" +
+            "Processor Type: " + ProcessorType + "\n" +
+            "Processor Count: " + ProcessorCount + "\n" +
+            "Processor Frequency: " + ProcessorFrequency;
+    }
+}
