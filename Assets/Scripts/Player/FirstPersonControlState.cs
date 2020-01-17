5817e3a4cd83b572aad17d68226b85e5e330c86b
Swaelo <harleylaurie@rocketmail.com>
2020-01-18 08:37:23 +1000

    stil mid-perfecting the fps camera controls

--- Assets/Scripts/Player/FirstPersonControlState.cs
+++ Assets/Scripts/Player/FirstPersonControlState.cs
@@ -19,8 +19,15 @@ public class FirstPersonControlState : State
 
     protected override void OnStateEnter()
     {
-        //Move the camera to the FPS position and parent it to the player when entering this state
+        //Reset the current camera tilt value
+        Controller.FirstPersonCameraTilt = 0f;
+
+        //Move the camera to the FPS position
         Controller.CameraTransform.position = Controller.FPSCameraPivot.transform.position;
+
+        //Face the camera forward in the direction the player is facing
+        Vector3 FacePosition = Controller.CameraTransform.position + Controller.transform.forward;
+        Controller.CameraTransform.LookAt(FacePosition);
     }
 
     protected override void OnStateExit()
@@ -46,15 +53,35 @@ public class FirstPersonControlState : State
 
     protected override void OnStateLateUpdate()
     {
-        //Pivot the camera up/down with vertical mouse movement
-        float CameraTiltAdjustment = Input.GetAxis("Mouse Y") * Controller.FirstPersonTiltSpeed * Controller.MouseDampening;
-        float NewCameraTilt = Controller.FirstPersonCameraTilt + CameraTiltAdjustment;
-        if (NewCameraTilt >= Controller.FirstPersonMinCameraTilt && NewCameraTilt <= Controller.FirstPersonMaxCameraTilt)
-        {
-            //Apply the new tilt adjustment if its within the accepted values
-            Controller.CameraTransform.RotateAround(-Controller.CameraTransform.right, CameraTiltAdjustment * Time.deltaTime);
-            Controller.FirstPersonCameraTilt = NewCameraTilt;
-        }
+        //Camera movement is not possible while the cursor is not locked to the screen
+        if (!Controller.CursorLocked)
+            return;
+
+        //Get new camera tilt values based on users mouse movement, make sure it stays clamped within accepted values
+        float NewCameraTilt = ClampTiltValue(Controller.FirstPersonCameraTilt + Input.GetAxis("Mouse Y") * Controller.FirstPersonTiltSpeed * Controller.MouseDampening);
+
+
+        ////Pivot the camera up/down with vertical mouse movement
+        //float CameraTiltAdjustment = Input.GetAxis("Mouse Y") * Controller.FirstPersonTiltSpeed * Controller.MouseDampening;
+        //float NewCameraTilt = Controller.FirstPersonCameraTilt + CameraTiltAdjustment;
+        //if (NewCameraTilt >= Controller.FirstPersonMinCameraTilt && NewCameraTilt <= Controller.FirstPersonMaxCameraTilt)
+        //{
+        //    //Apply the new rotation values onto the camera
+        //    Controller.CameraTransform.position = Controller.FPSCameraPivot.transform.position;
+        //    Controller.CameraTransform.LookAt(Controller.CameraTransform.position + Controller.transform.forward);
+        //    Controller.CameraTransform.Rotate(-Controller.CameraTransform.right, NewCameraTilt);
+
+        //    ////Use the camera dummy to get the perfect position/rotation for the new camera values
+        //    //Controller.CameraDummy.transform.position = Controller.FPSCameraPivot.transform.position;
+        //    //Controller.CameraDummy.transform.LookAt(Controller.CameraDummy.transform.position + Controller.transform.forward);
+        //    //Controller.CameraDummy.transform.Rotate(-Controller.CameraDummy.transform.right, NewCameraTilt);
+
+
+
+        //    ////Apply the new tilt adjustment if its within the accepted values
+        //    //Controller.CameraTransform.RotateAround(-Controller.CameraTransform.right, CameraTiltAdjustment * Time.deltaTime);
+        //    //Controller.FirstPersonCameraTilt = NewCameraTilt;
+        //}
 
         //Check for user scrolling the mousewheel back out to return to the third person mode
         float CameraZoom = Input.GetAxis("Mouse ScrollWheel");
@@ -63,6 +90,18 @@ public class FirstPersonControlState : State
             Controller.StateMachine.SetState(GetComponent<ThirdPersonFreeControlState>());
             return;
         }
+    }
+
+    //Returns tilt value kept within accepted parameters
+    private float ClampTiltValue(float CurrentValue)
+    {
+        //Stop value exceeding min/max values, then return it
+        float ClampedValue = CurrentValue;
+        if (ClampedValue > Controller.FirstPersonMaxCameraTilt)
+            ClampedValue = Controller.FirstPersonMaxCameraTilt;
+        if (ClampedValue < Controller.FirstPersonMinCameraTilt)
+            ClampedValue = Controller.FirstPersonMinCameraTilt;
+        return ClampedValue;
     }
 
     //Computes a new movement vector based on the users input
